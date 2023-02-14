cca8bbb9170108c160ce20733e240d10077b9f71
Roxydog21 <reesepavo@gmail.com>
2023-02-13 17:46:33 -0800

    Added Arm stuff to Robo con and ArmDown

--- FRC-2023/src/main/java/frc/robot/commands/IntakeArm/ArmDown.java
+++ FRC-2023/src/main/java/frc/robot/commands/IntakeArm/ArmDown.java
@@ -7,36 +7,23 @@ package frc.robot.commands.IntakeArm;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Arm;
 
-public class GoToAngle extends CommandBase {
-  /** Creates a new GoToAngle. */
-
-  private static final class Config{
-    public static final double kP = 0.1;
-  }
-
+public class ArmDown extends CommandBase {
+  /** Creates a new ArmDown. */
   private Arm m_arm;
-
-  private double setpoint;
-  private double initialTicks;
-  
-  public GoToAngle(Arm arm, double angle) {
-      
-    m_arm = arm;
-    setpoint = angle;
-    
+  public ArmDown() {
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_arm);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    initialTicks = m_arm.getEncoderTicks();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_arm.setSpeed(Config.kP*(setpoint - m_arm.getEncoderTicks()));
+    if(m_arm.getLowerLimit()) {m_arm.setSpeed(-.05);}
+    else m_arm.setSpeed(0);}
   }
 
   // Called once the command ends or is interrupted.
@@ -48,6 +35,6 @@ public class GoToAngle extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+  return m_arm.getLowerLimit();
   }
 }
